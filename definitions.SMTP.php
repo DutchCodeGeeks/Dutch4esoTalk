360f05728f52e7282969625c1eadb8785e3b3588
tomsmeding <tomsmeding@users.noreply.github.com>
2014-04-11 20:34:05 +0200

    Update definitions.SMTP.php

--- definitions.SMTP.php
+++ definitions.SMTP.php
@@ -1,12 +1,13 @@
 <?php
 // Copyright 2013 Toby Zerner, Simon Zerner
+// Edited by tomsmeding, tkon99
 // This file is part of esoTalk. Please see the included license file for usage information.
 
-// English Definitions for the SMTP plugin.
+// Dutch Definitions for the SMTP plugin.
 
 $definitions["Server"] = "Server";
-$definitions["Port"] = "Port";
-$definitions["Authentication"] = "Authentication";
-$definitions["Normal"] = "Normal";
+$definitions["Port"] = "Poort";
+$definitions["Authentication"] = "Authenticatie";
+$definitions["Normal"] = "Normaal";
 $definitions["TLS"] = "TLS";
 $definitions["SSL"] = "SSL";
